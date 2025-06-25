91215d4caa5ceb110018a56a89e3fd6a23463cca
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-06-25 10:36:01 +0530

    How to add Css and take a Value Using Field Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Profile],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected title = 'angular-part2';
+  protected title = 'Angular part 2';
+
+  // handleEventClick(event: Event) {
+
+  // console.log('Function Called', event.type);
+  // console.log("Value",(event.target as HTMLInputElement).value)
+  // console.log('Function Called', (event.target as Element).className);
+  // }
+
+  // ---------------------------
+
+  name = '';
+  displayName = '';
+  email = '';
+
+  getName(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  ShowName() {
+    this.displayName = this.name;
+  }
+
+  setName() {
+    this.name = 'Subham';
+  }
+
+  getEmail(val: string) {
+    console.log(val);
+    this.email = val;
+  }
+  setEmail() {
+    this.email = 'default@test.com';
+  }
 }
